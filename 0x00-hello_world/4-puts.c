2d0b67cf7c6df1294db6bbf4645f2b7aad85bfae
onyigloria <onyeenog@gmail.com>
2022-10-13 12:03:44 -0700

    Hello,put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Printing
- *
- *Return: Always 0 (Successful)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
-int main(void)
+
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	retun(0);
+		return (0);
 }
