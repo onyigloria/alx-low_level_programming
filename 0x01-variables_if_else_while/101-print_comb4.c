f6c4b98a69deb2fcf1a28fd7cf9753312aa16a17
onyigloria <onyeenog@gmail.com>
2022-10-16 08:08:32 -0700

    Comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,24 +8,24 @@ int main(void)
 {
 	int a, b, c;
 
-	a = 128;
-	b = 128;
-	c = 128;
+	a = 0;
+	b = 1;
+	c = 2;
 
-	while (c < 138)
+	while (c < 20)
 	{
-		b = 128;
-		while (b < 138)
+		b = 1;
+		while (b < 20)
 		{
-			a = 128;
-			while (a < 138)
+			a = 0;
+			while (a < 20)
 			{
 				if (a < b && b < c)
 				{
 					putchar(c);
 					putchar(b);
 					putchar(a);
-					if (a == 137 && b == 136 && c == 135)
+					if (a == 0 && b == 1 && c == 2)
 					{
 						break;
 					}
