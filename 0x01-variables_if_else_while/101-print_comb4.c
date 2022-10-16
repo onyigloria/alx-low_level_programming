edad2be2f4746b5ec69572a6bec018102684a2cb
onyigloria <onyeenog@gmail.com>
2022-10-16 09:52:04 -0700

    Comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,10 +9,10 @@ int main(void)
 }
 	char h = '0', t = '1', u = '2';
 
-	while ((h <= '7') || (t <= '8') || (u <= '9')) {
-		printf ("%c%c%c, ", h, t, u);
+	while ((h <= '7') || (t <= '8') || (u <= '9'))
+	{
+		printf("%c%c%c, ", h, t, u);
 		u != '9' ? ++u : (t != '8' ? (++t, u = t + 1) : (++h, t = h + 1, u = t + 1));
 	}
-	
-	return (0);
+return (0);
 }
