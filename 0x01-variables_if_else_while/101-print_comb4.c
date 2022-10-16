03d09d92888dc962e8d14d61a077eb059348acea
onyigloria <onyeenog@gmail.com>
2022-10-16 08:29:38 -0700

    Comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,19 +12,19 @@ int main(void)
 	b = 1;
 	c = 2;
 
-	while (c <= 9)
+	while (a <= 7)
 	{
 		b = 1;
 		while (b <= 8)
 		{
-			a = 0;
-			while (a <= 7)
+			c = 2;
+			while (c <= 9)
 			{
 				if (a != b && a != c && b != c)
 				{
-					putchar(c + '0');
-					putchar(b + '0');
 					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
 					if (a == 0 && b == 1 && c == 2)
 					{
 						break;
