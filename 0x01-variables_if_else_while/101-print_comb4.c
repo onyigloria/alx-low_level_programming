5ff1634937a18bc4d026e516bbaaf4306f160496
onyigloria <onyeenog@gmail.com>
2022-10-16 09:46:01 -0700

    Comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,43 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
-{
-	int a, b, c;
+}
+	char h = '0', t = '1', u = '2';
 
-	a = 0;
-	b = 1;
-	c = 2;
-
-	while (c <= 9)
-	{
-		b = 1;
-		while (b <= 8)
-		{
-			a = 0;
-			while (c <= 7)
-			{
-				if (a != b && a != c && b != c)
-				{
-					putchar(a + '0');
-					putchar(b + '0');
-					putchar(c + '0');
-					if (a == 0 && b == 1 && c == 2)
-					{
-						break;
-					}
-					putchar(',');
-					putchar(' ');
-				}
-				a++;
-			}
-			b++;
-		}
-		c++;
+	while ((h <= '7') || (t <= '8') || (u <= '9')) {
+		printf ("%c%c%c, ", h, t, u);
+		u != '9' ? ++u : (t != '8' ? (++t, u = t + 1) : (++h, t = h + 1, u = t + 1));
 	}
-	putchar('\n');
+	
 	return (0);
 }
