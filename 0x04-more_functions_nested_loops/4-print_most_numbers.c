21ed9c60c19e0b9edc915d238936ce3d40176e62
onyigloria <onyeenog@gmail.com>
2022-10-20 06:33:28 -0700

    Most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*print_most_numbers - a function that prints the numbers, from 0 to 9, followed by a new line
+*print_most_numbers - print most numbers from 0 to 9
 *Return: 0
 */
 
