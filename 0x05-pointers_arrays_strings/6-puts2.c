8429d2057a61cf6e8ff656a3b3d3066741c2d5a4
onyigloria <onyeenog@gmail.com>
2022-10-24 11:17:47 -0700

    Puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ *
+ * Return: 0 is success
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	if (i % 2 == 0)
+		_putchar(str[i]);
+	_putchar('\n');
+}
