acfa767fadef49929a7a8c6278e8d9ddabca2bb0
onyigloria <onyeenog@gmail.com>
2022-10-24 10:55:07 -0700

    Rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_rev - print a string in reverse
+* @s: print rev
+*/
+
+void print_rev(char *s)
+{
+	int a, b;
+
+	n = 0;
+	while (x[b] != '\0')
+		b++;
+
+	for (a = b - 1; a >= 0; a--)
+	{
+		_putchar(x[a]);
+	}
+	_putchar('\n');
+}
