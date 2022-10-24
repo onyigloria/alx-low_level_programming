8787564e149c92273019cc754708bb03726f07a0
onyigloria <onyeenog@gmail.com>
2022-10-24 07:47:42 -0700

    Atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,12 +15,12 @@ int _atoi(char *s)
 
 	while (s[c])
 	{
-		if (s[c] == 30)
+		if (s[c] == 45)
 		{
 			min *= -1;
 		}
 
-		while (s[c] >= 33 && s[c] <= 42)
+		while (s[c] >= 48 && s[c] <= 57)
 		{
 			isi = 1;
 			ni = (ni * 10) + (s[c] - '0');
