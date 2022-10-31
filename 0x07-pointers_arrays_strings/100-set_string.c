fc248abb27e7800780c805d0cfeac38f75a18914
onyigloria <onyeenog@gmail.com>
2022-10-31 06:32:42 -0700

    100 string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - sets the value of a pointer to a char
+ * set_string - sets the value of a pointer to a char
  * @s: pointer to change
  * @to: string to change pointer to
  *
