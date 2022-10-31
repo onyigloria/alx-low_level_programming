90b0be08669bb1273091081efee875a97c9cd27e
onyigloria <onyeenog@gmail.com>
2022-10-31 08:12:38 -0700

    Memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area
+ *
+ * @n: function copies
+ * @src: bytes from memory area
+ * @dest: to memory area
+ *
+ * Return: 0
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (0);
+}
